8ae94978a75d296b0f3d597bd643a88f9620a88c
SamLannister <icetexy@gmail.com>
2018-07-14 05:13:26 +0100

    dsfsdf

---
+++ PBO's/Trident Menu/nasdn8dndna8nd8and28/config.cpp
@@ -0,0 +1,28 @@
+class CfgPatches
+{
+	class nasdn8dndna8nd8and28
+	{
+		name = "n";
+		author = "i";
+		url = "http://gger.com";
+		requiredVersion = 1.60; 
+		requiredAddons[] = {"A3_Functions_F"};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+class CfgFunctions
+{
+	class ffffffffffffzfffffffffffffffffffffffaggit
+	{
+		class ffffffffffffzfffffffffffffffzfffffffaggit
+		{
+			class Init
+			{
+				postInit=1;
+				file = "\nasdn8dndna8nd8and28\sadsadsdasad\fn_pos2.sqf";
+			};
+		};
+	};
+};
